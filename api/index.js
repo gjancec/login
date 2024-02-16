26e5674ed43d95e984a2bd13e2013e8bc316440c
gjancec <gjancec@gmail.com>
2024-02-16 08:55:29 +0100

    Connect to express and Mongo DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 //connect to express app
 const app=express();
 
+//connect to MongoDB
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 
