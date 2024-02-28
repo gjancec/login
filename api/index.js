cc1090a74ba9671b6072f8b231e5cbbb47a4560f
gjancec <gjancec@gmail.com>
2024-02-28 06:58:10 +0100

    cors

--- api/index.js
+++ api/index.js
@@ -18,6 +18,12 @@ app.use(cors(
    credentials:true
   }
 ));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 
 // for testing post json from browser to api
